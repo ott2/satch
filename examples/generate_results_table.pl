026e112ff9e38a4195def86eb9b5bbdc18d7314d
Joan Espasa Arxer <cat.os.mandros@gmail.com>
2022-04-26 08:26:42 +0100

    dirty script to run solvers

--- examples/generate_results_table.pl
+++ examples/generate_results_table.pl
@@ -4,11 +4,20 @@ use strict;
 use warnings;
 # Quick and dirty script to run all existing graphs sequencially
 # on a list of MaxSAT methods to see how they scale.
+#
+
+# From the basename of a file extract number of vertices and seed
+sub data_from_basename {
+    my $basename = shift;
+    $basename =~ m/graph-(\d*)-(\d*)/g;
+    return $1, $2;
+}
 
 my $methods = {
     'open-wbo 1' => 'open-wbo -algorithm=1',
     'open-wbo 5' => 'open-wbo -algorithm=5',
-    'maxhs'      => 'maxhs'
+    'maxhs'      => 'maxhs',
+    'EvalMaxSAT' => 'EvalMaxSAT_bin'
     #'evalmaxsat' => ''
 };
 
@@ -19,17 +28,35 @@ foreach my $file (glob("dis/*.wdimacs"), glob("con/*.wdimacs")){
     push @graphs, $basename;
 }
 
+my @results;
 # foreach method, execute it for all graphs
 foreach my $key (keys %$methods) {
     # foreach file
     foreach my $graph (@graphs) {
+        my ($vertices, $seed) = data_from_basename($graph);
+
+        # execute and time the command
         my $begin_time = time();
-        # execute the command
         my $out = qx/$methods->{$key} $graph.wdimacs/;
-        #
         my $end_time = time();
         my $elapsed = sprintf("%.2f", $end_time - $begin_time);
-        print "$key, $graph.wdimacs, $elapsed \n";
-    }
 
+        # store results
+        push @results, {"key" => $key,
+                        "vertices" => $vertices,
+                        "seed" => $seed,
+                        "file" => "$graph.wdimacs",
+                        "time" => $elapsed};
+    }
+}
+
+# sort correctly to plot
+@results = sort {
+    $a->{"key"} cmp $b->{"key"} ||
+    $a->{"vertices"} <=> $b->{"vertices"}
+} @results;
+
+# finally print results
+foreach my $result (@results){
+    print "$result->{'key'}, $result->{'file'}, $result->{'time'} \n";
 }
